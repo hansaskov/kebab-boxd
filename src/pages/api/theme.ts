103ebedaa71e937ecdab74298968fb6d70f661d2
Hans Askov <hans@askov.dk>
2026-08-05 23:27:49 +0200

    Remove cache after changing the theme

--- src/pages/api/theme.ts
+++ src/pages/api/theme.ts
@@ -2,16 +2,27 @@ import { eq } from "drizzle-orm";
 import { s } from "@src/db/index";
 import type { APIRoute } from "astro";
 
-export const POST = (async ({ redirect, locals, request }) => {
-	const session = locals.session;
+export const POST = (async ({ redirect, locals: {db, session}, request }) => {
+
 	if (!session) {
 		return redirect("/login");
 	}
-
 	const newTheme = session.theme === "dark" ? "light" : "dark";
 
-	await locals.db.update(s.sessions).set({ theme: newTheme }).where(eq(s.sessions.id, session.id));
+	await db
+		.update(s.sessions)
+		.set({ theme: newTheme })
+		.where(eq(s.sessions.id, session.id));
 
 	const referer = request.headers.get("Referer");
-	return redirect(referer ?? "/");
+
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: referer ?? "/",
+			"Cache-Control": "no-store",
+			"Clear-Site-Data": "\"cache\"",
+		},
+	});
+
 }) satisfies APIRoute;
